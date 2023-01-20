3f459eac31dac494bcaa1dd55e7622d0120e1e8a
Zyam <zyammaqsood7@gmail.com>
2023-01-20 15:02:44 +0500

    SumApp

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, total;
+    cout << "Enter First Number" << endl;
+    cin >> num1;
+    cout << "Enter Second Number" << endl;
+    cin >> num2;
+    total  = num1 + num2;
+    cout << "The sum is " << total;
+    return 0;
+}
